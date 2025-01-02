19a5cef486a55cfa8007e9cb5c8bce909e798966
wiggum220 <6446397+wiggum220@users.noreply.github.com>
2025-01-01 23:11:00 -0500

    update default fmt

--- HXVideo.py
+++ HXVideo.py
@@ -7,7 +7,7 @@ def main():
     output_group = parser.add_mutually_exclusive_group()
     input_group.add_argument('-i', '-input', help='Input file: The HX file you want to convert.')
     output_group.add_argument('-o', '-output', help='Output file: The output file you want to create.')
-    parser.add_argument('-fmt', help='Output format: The format you want to convert to.')
+    parser.add_argument('-fmt', default='mkv', help='Output format: The format you want to convert to.')
     input_group.add_argument('-indir', help='Input directory: The directory containing the HX files you want to convert.')
     output_group.add_argument('-outdir', help='Output directory: The directory where you want to save the converted files.')
     parser.add_argument('-r', action="store_true", help='Recursive mode: Process subdirectories and their contents.')
